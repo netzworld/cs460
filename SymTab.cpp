941f8c30d5b5cb8eae71a73795c6c07f1dfcb5de
Nic Randazzo <44889688+netzworld@users.noreply.github.com>
2023-04-27 13:28:03 -0700

    Update SymTab.cpp

--- SymTab.cpp
+++ SymTab.cpp
@@ -4,11 +4,12 @@
 
 #include <iostream>
 #include "SymTab.hpp"
-
-void SymTab::setValueFor(const std::string &vName, int value) {
+//                                                
+void SymTab::setValueFor(const std::string &vName, TypeDescriptor* value) {
     // Define a variable by setting its initial value.
     if(debug)
         std::cout << vName << " <- " << value << std::endl;
+     
     symTab[vName] = value;
 }
 
@@ -16,7 +17,7 @@ bool SymTab::isDefined(const std::string &vName) {
     return symTab.find(vName) != symTab.end();
 }
 
-int SymTab::getValueFor(const std::string &vName) {
+TypeDescriptor* SymTab::getValueFor(const std::string &vName) {
     if( ! isDefined(vName)) {
         std::cout << "SymTab::getValueFor: " << vName << " has not been defined.\n";
         exit(1);
